b7f77e9e2ab1be7d07c2e1fbf68ade3b5f8d85b7
coalpha <22552895+coalpha@users.noreply.github.com>
2020-09-23 04:27:50 -0700

    lab2 is confusing
    I really have no idea what I'm doing here so I'll ask the other
    people tomorrow anyways.

---
+++ lab/lab2/name.cpp
@@ -0,0 +1,19 @@
+// This program will write the name, address and telephone
+// number of the programmer.
+
+// Cole Gannon
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout <<
+		"************\n"
+		"    Programmer: Cole Gannon\n"
+		"                7880 McClellan Rd\n"
+		"                Cupertino, CA. 95014\n"
+		"\n"
+		"     Telephone: 408-257-9985\n"
+		"\n"
+		"************\n";
+}
