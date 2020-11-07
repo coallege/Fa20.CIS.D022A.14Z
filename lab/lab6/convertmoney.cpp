199f8533e2d7eafce0e4c2535a22b7ff3e120d68
coalpha <22552895+coalpha@users.noreply.github.com>
2020-11-07 00:26:03 -0800

    The Packed Variadic Template of Doom

--- lab/lab6/convertmoney.cpp
+++ lab/lab6/convertmoney.cpp
@@ -1,60 +1,71 @@
 #include <iostream>
 #include <iomanip>
+#include <string>
 using namespace std;
-
-// This program will input American money and convert it to foreign currency
 
 // Cole Gannon
 
-class Money {
-	using t = double;
+#define comptime static constexpr auto
 
-	// currency pairs
-	static constexpr auto USD_EUR{1.06};
-	static constexpr auto USD_MXN{9.73};
-	static constexpr auto USD_YEN{124.35};
+struct EUR {
+	comptime s{"euros"};
+	comptime USD_X{1.06};
+};
 
-public:
-	t dollars;
-	t euros;
-	t pesos;
-	t yen;
-	class EUR{};
-	class MXN{};
-	class YEN{};
+struct MXN {
+	comptime s{"pesos"};
+	comptime USD_X{9.73};
+};
 
-	Money(t dollars) {
-		this->dollars = dollars;
-		this->euros   = dollars * Money::USD_EUR;
-		this->pesos   = dollars * Money::USD_MXN;
-		this->yen     = dollars * Money::USD_YEN;
+struct YEN {
+	comptime s{"yen"};
+	comptime USD_X{124.35};
+};
+
+template <class A>
+void what() {
+	cout << A::s;
+}
+
+template<class A, class B>
+void what() {
+	cout << A::s << " and " << B::s;
+}
+
+template<class A, class B, class C>
+void what() {
+	cout << A::s << ", " << B::s << ", and " << C::s;
+}
+
+// https://stackoverflow.com/questions/19182425
+// <3
+template <class ...Cs>
+void convert_usd_to() {
+	comptime CURRENCY_WIDTH{7};
+
+	cout << "Convert USD to "; what<Cs...>();
+	cout << ':';
+
+	double usd;
+	string line;
+
+	prompt_usd:
+	cout << "\n" << setw(CURRENCY_WIDTH) << "dollars" << ": ";
+	getline(cin, line);
+
+	try {
+		usd = {stod(line)};
+	} catch (invalid_argument e) {
+		cout << "Unable to parse \"" << line << "\", please try again.";
+		goto prompt_usd;
+	} catch (out_of_range e) {
+		cout << line << "Is too large or too small of a value. Please try again.";
+		goto prompt_usd;
 	}
 
-	template<class CUR>
-	void dump1();
-
-	template<>
-	void dump1<Money::EUR>() {
-		cout << "euros  : " << this->euros << '\n';
-	}
-
-	template<>
-	void dump1<Money::MXN>() {
-		cout << "pesos  : " << this->pesos << '\n';
-	}
-
-	template<>
-	void dump1<Money::YEN>() {
-		cout << "yen    : " << this->yen << '\n';
-	}
-
-	template<class ...CURs>
-	void dump() {
-		cout << "--------------------------------\n";
-		cout << "dollars: " << this->dollars << '\n';
-		auto _ = {(this->dump1<CURs>(), 0)...};
-		cout << "--------------------------------\n";
-	}
+	// my favorite line of C++ that I've written so far
+	{ auto _ = {(cout << setw(CURRENCY_WIDTH) << Cs::s << ": " << usd * Cs::USD_X << '\n', 0)...}; }
+	cout << '\n';
 };
 
 int main() {
@@ -62,43 +73,9 @@ int main() {
 
 	cout << fixed << showpoint << setprecision(2);
 
-	cout <<
-		"Please input the amount of American Dollars\n"
-		"you want converted to euros and pesos\n> ";
-	cin >> dollars;
-
-	Money first(dollars);
-	first.dump<Money::EUR, Money::MXN>();
-
-	cout <<
-		"Please input the amount of American Dollars\n"
-		"you want converted to euros, pesos, and yen\n> ";
-	cin >> dollars;
-
-	Money second(dollars);
-	second.dump<Money::EUR, Money::MXN, Money::YEN>();
-
-	cout <<
-		"Please input the amount of American Dollars\n"
-		"you want converted to yen\n> ";
-	cin >> dollars;
-
-	Money third(dollars);
-	third.dump<Money::YEN>();
-
-	cout <<
-		"Please input the amount of American Dollars\n"
-		"you want converted to euros\n> ";
-	cin >> dollars;
-
-	Money fourth(dollars);
-	fourth.dump<Money::EUR>();
-
-	cout <<
-		"Please input the amount of American Dollars\n"
-		"you want converted to pesos\n> ";
-	cin >> dollars;
-
-	Money fifth(dollars);
-	fifth.dump<Money::MXN>();
+	convert_usd_to<EUR, MXN>();
+	convert_usd_to<EUR, MXN, YEN>();
+	convert_usd_to<YEN>();
+	convert_usd_to<EUR>();
+	convert_usd_to<MXN>();
 }
